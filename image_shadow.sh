4a27d9f4a9539f6a7cbe8e6fd5c4c88420c32789
yKesamaru <y.kesamaru@tokai-kaoninsho.com>
2022-01-09 19:26:02 +0900

    a

--- image_shadow.sh
+++ image_shadow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 convert graph_2_no_shadow.png \
-    \( +clone -background black -shadow 10x10+1+1 \) \
+    \( +clone -background black -shadow 10x20+1+1 \) \
     +swap -background none -layers merge +repage \
     output.png
